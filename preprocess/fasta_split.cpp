1f1cb08f763aec025378ffecb44eb705b201a184
Mitsuki <mitsuo.com.mu@gmail.com>
2017-04-25 19:10:25 +0900

    daily

--- preprocess/fasta_split.cpp
+++ preprocess/fasta_split.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <seqan/seq_io.h>
 
@@ -14,6 +15,7 @@ int main(int argc, char** argv){
     seqan::String<char> seqFilepath=argv[1];
 	seqan::String<char> chrFilepath=argv[2];
     seqan::String<char> plsFilepath=argv[3];
+    std::string logFilepath=argv[4];
     
     //read input file
     seqan::StringSet<seqan::CharString > ids;
@@ -48,6 +50,9 @@ int main(int argc, char** argv){
     //output
     int chrCount=0;
     int plsCount=0;
+    std::stringstream chrSs;
+    std::stringstream plsSs;
+
     for(unsigned i=0;i<length(ids);i++){
         std::string id_str=seqan::toCString(ids[i]);
         std::string::size_type index=id_str.find("plasmid");
@@ -55,15 +60,36 @@ int main(int argc, char** argv){
         if(index==std::string::npos){
 		    seqan::writeRecord(chrFileOut,ids[i],seqs[i]);
             chrCount++;
+            chrSs<<seqan::length(seqs[i])<<":";
         }
         else{
 		    seqan::writeRecord(plsFileOut,ids[i],seqs[i]);
             plsCount++;
+            plsSs<<seqan::length(seqs[i])<<":";
         }
     }
     cout<<"DONE outputing "<<chrCount<<" seqs to "<<chrFilepath<<"."<<endl;
     cout<<"DONE outputing "<<plsCount<<" seqs to "<<plsFilepath<<"."<<endl;
 
+    //delete last ':'
+    std::string chrLength=chrSs.str();
+    std::string plsLength=plsSs.str();
+    if(chrCount>0){
+        chrLength.pop_back();
+    }
+    if(plsCount>0){
+        plsLength.pop_back();
+    }
+
+    //write log function
+    std::ofstream ofs(logFilepath);
+    if (!ofs){
+		cerr<<"ERROR: Could not open "<<logFilepath<<"."<<endl;;
+		return 1;
+    }
+    
+    ofs<<"test"<<","<<"chromosome"<<","<<chrFilepath<<","<<chrCount<<","<<chrLength<<endl;
+    ofs<<"test"<<","<<"plasmid"<<","<<plsFilepath<<","<<plsCount<<","<<plsLength<<endl;
 
     return 0;
 }
