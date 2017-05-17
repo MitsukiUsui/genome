baccd8ac68e78c35541a4ccd9c9df47f223baf0f
Mitsuki <mitsuo.com.mu@gmail.com>
2017-05-17 11:43:57 +0900

    DONE visualizing count

--- patternanalyze/pattern_transition.sh
+++ patternanalyze/pattern_transition.sh
@@ -8,9 +8,9 @@ basename=${1}
 realFilepath=${seqDir}${basename}_chromosome.fna
 simuFilepath=${seqDir}${basename}_chromosome_simu${shuffleMode1}${shuffleMode2}_0.fna
 windowSize_bp=500
-countFilepath=${outDir}count/${basename}_chromosome_simu${shuffleMode}${shuffleMode2}_count.csv
-transFilepath=${outDir}trans/${basename}_chromosome_simu${shuffleMode}${shuffleMode2}_trans.csv
-transdegFilepath=${outDir}transdeg/${basename}_chromosome_simu${shuffleMode}${shuffleMode2}_transdeg.csv
+countFilepath=${outDir}count/${basename}_chromosome_simu${shuffleMode1}${shuffleMode2}_count.csv
+transFilepath=${outDir}trans/${basename}_chromosome_simu${shuffleMode1}${shuffleMode2}_trans.csv
+transdegFilepath=${outDir}transdeg/${basename}_chromosome_simu${shuffleMode1}${shuffleMode2}_transdeg.csv
 
 echo ${basename}
 ~/altorf/genome/patternanalyze/pattern_transition.py ${realFilepath} ${simuFilepath} ${windowSize_bp} ${countFilepath} ${transFilepath} ${transdegFilepath}
--- patternanalyze/uge_pattern_transition.sh
+++ patternanalyze/uge_pattern_transition.sh
@@ -3,11 +3,11 @@ input="../speciespick/picked_assembly_summary.csv"
 
 array=(`awk -F, 'NR>1 {print $4}' ${input}`) 
 
-logDir="/home/mitsuki/out/altorf/genome/patternanalyze/log/"
+logDir="/home/mitsuki/out/altorf/genome/patternanalyze/log"
 
 for basename in ${array[@]}
 do
-	outlog=${logDir}sge_${basename}.out
-	errlog=${logDir}sge_${basename}.err
+	outlog=${logDir}/sge_${basename}.out
+	errlog=${logDir}/sge_${basename}.err
 	qsub -S /bin/bash -q all.q -o ${outlog} -e ${errlog} ./pattern_transition.sh ${basename}
 done
