f890fcbbcf7ab3beadec1ad85c5f59ab048c732d
MitsukiUsui <mitsuo.com.mu@gmail.com>
2017-10-06 20:03:12 +0900

    ISSUE-10: add checker.py

---
+++ preprocess/script/uge/checker.py
@@ -0,0 +1,14 @@
+import sys
+
+sys.path.append("/home/mitsuki/altorf/genome/db")
+from dbcontroller import DbController
+
+def main(dbFilepath):
+    dc = DbController(dbFilepath)
+    taxid_lst = dc.get_target("preprocess")
+    dc.mark_as_done(taxid_lst, "preprocess")
+
+
+if __name__ == "__main__":
+    dbFilepath = "/home/mitsuki/altorf/genome/db/altorf.db"
+    main(dbFilepath)
