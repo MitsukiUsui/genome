b3c8d5a9beb633417936187eadedb701f8ffad28
MitsukiUsui <mitsuo.com.mu@gmail.com>
2017-10-06 19:35:00 +0900

    ISSUE-10: submitter.py, caller.sh

---
+++ preprocess/script/uge/caller.sh
@@ -0,0 +1,8 @@
+taxid=${1}
+seqFilepath=/data/mitsuki/data/refseq/genomic_fna/${taxid}_genomic.fna
+chrFilepath=/data/mitsuki/out/altorf/genome/fasta/${taxid}_chr.fna
+plsFilepath=/data/mitsuki/out/altorf/genome/fasta/${taxid}_pls.fna
+logFilepath=/data/mitsuki/out/altorf/genome/preprocess/split/${taxid}.log
+
+/home/mitsuki/altorf/genome/preprocess/build/fasta_split ${seqFilepath} ${chrFilepath} ${plsFilepath} ${logFilepath}
+
