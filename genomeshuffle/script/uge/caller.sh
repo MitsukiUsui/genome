3fd614d4bddfb5cac61779ee938f6b3cabd95bce
MitsukiUsui <mitsuo.com.mu@gmail.com>
2017-11-05 18:15:17 +0900

    ISSUE-30: update uge

--- genomeshuffle/script/uge/caller.sh
+++ genomeshuffle/script/uge/caller.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -N shuffle
+#$ -q standard.q
+#$ -cwd
+#$ -v PATH
+#$ -o ./log/$JOB_ID.out
+#$ -e ./log/$JOB_ID.err
+#$ -l mem_free=1G
+
 taxid=${1}
 seqFilepath=/data/mitsuki/out/altorf/genome/fasta/${taxid}_chr.fna
 gffFilepath=/data/mitsuki/data/refseq/genomic_gff/${taxid}_genomic.gff
@@ -5,5 +15,6 @@ outFilepath=/data/mitsuki/out/altorf/genome/fasta/${taxid}_sim.fna
 shuffleMode1=0
 shuffleMode2=3
 
-time /home/mitsuki/altorf/genome/genomeshuffle/build/shuffle_genome ${seqFilepath} ${gffFilepath} ${outFilepath} ${shuffleMode1} ${shuffleMode2}
+time ../../build/shuffle_genome \
+     ${seqFilepath} ${gffFilepath} ${outFilepath} ${shuffleMode1} ${shuffleMode2}
 
--- genomeshuffle/src/shuffle_genome.cpp
+++ genomeshuffle/src/shuffle_genome.cpp
@@ -114,19 +114,19 @@ int main(int argc, char **argv) {
     }
 
     update_genetic_code(geneticCode, myCDS_vecvec, seqs);
-    geneticCode.__show_freq(true);
+    //geneticCode.__show_freq(true);
 
     std::vector<ShuffleRegion> shuffleRegions;
     get_shuffle_region(shuffleRegions, shuffleMode, seqs, myCDS_vecvec);
-    cout << "DONE: find " << seqan::length(shuffleRegions) << " shuffle regions" << endl;
+    cout << "DONE: find " << shuffleRegions.size() << " shuffle regions" << endl;
 
     shuffle_genome(seqs, shuffleRegions, geneticCode);
-
-    geneticCode.clear_count();
-    update_genetic_code(geneticCode, myCDS_vecvec, seqs);
-    geneticCode.__show_freq(true);
-
     write_fasta(seqIds, seqs, outFilepath);
     cout << "DONE: output to " << outFilepath << endl;
+
+    //geneticCode.clear_count();
+    //update_genetic_code(geneticCode, myCDS_vecvec, seqs);
+    //geneticCode.__show_freq(true);
+
     return 0;
 }
